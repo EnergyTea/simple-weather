b3af8d870a387a6c8685e44ae066cfb0096f98d6
Руслан Васильев <rusvas15@yandex.ru>
2020-10-26 01:17:59 +0300

    Добавил отображение погоды
    Полученного через API

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import WeatherDisplay from './WeatherDisplay';
+import PLACES from './CITYS';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activePlace: 0,
+    };
+  }
+  render() {
+    const activePlace =  this.state.activePlace;
+    return (
+      <div className="App">
+        <WeatherDisplay zip={"12345"} />
+        {PLACES.map((PLACES, index) => (
+          <button
+            key={index}
+            onClick={() => {
+                console.log('Clicked index '+index);
+              }
+            }
+          >
+            {PLACES.name}
+          </button>
+        ))}
+        <WeatherDisplay
+          key={activePlace}
+          zip={PLACES[activePlace].zip}
+        />
+      </div>
+    );
+  }  
 }
 
 export default App;
